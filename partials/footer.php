0c01a5c479b728ec38f7e0803c99fe64932c8520
mouika-330 <lakshmimounikayadav@gmail.com>
2021-02-13 00:26:11 +0530

    added new css im and partials files and modified login and register and homepage

---
+++ partials/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer" style="margin-left: 0px;">
+    <div class="text-center">
+        <strong>Copyright &copy; 2021-2022 <a href="learningfromscratch.online">HappyTech</a>.</strong> All rights
+        reserved.
+    </div>
+</footer>
+<!-- /.control-sidebar -->
+<!-- Add the sidebar's background. This div must be placed
+     immediately after the control sidebar -->
+<div class="control-sidebar-bg"></div>
